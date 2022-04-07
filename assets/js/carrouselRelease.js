f45cb8ab9e9ec065c7f71b0c1e7582ab0ea75f98
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-04-07 09:46:13 +0200

    modification du modal + ajout de flèches + ajout des commentaires js

--- assets/js/carrouselRelease.js
+++ assets/js/carrouselRelease.js
@@ -1,14 +1,23 @@
+/**===========================================================
+ * Création de la fonction asynchrone + récupération de l'API
+ * ===========================================================
+ */
 const dataFetch = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=94d3b735c0a1582c1b3cb985eee421a1&primary_release_year=2022&language=fr-FR', {cache: "no-cache"})
     return await data.json();
 }
 
+
+/**===========================================================
+ * Création de sous-tableaux + remplissage avec les infos
+ * contenues dans l'API
+ * ===========================================================
+ */
 let posterCarrousel2 = [];
 let titleCarrousel2 = [];
 let voteCarrousel2 = [];
 let synopsisCarrousel2 = [];
 
-//je remplies des sous-tableaux avec les données que je récupère
 getData = (arr) => {
     return arr.results.forEach(el => {
         posterCarrousel2.push(el.poster_path);
@@ -18,28 +27,48 @@ getData = (arr) => {
     });
 }
 
+/**===========================================================
+ * Création de la promesse 
+ * ===========================================================
+ */
 dataFetch()
     .then(r => {
         getData(r);
 
         let nombreImg = titleCarrousel2.length;
         
-        //position par défaut du Carrousel
+        /**===========================================================
+         *  Position par défaut du carrousel
+         * ===========================================================
+         */
         pos = 0;
         
-        //manipulation du dom
+        /**===========================================================
+         * Récupération des éléments pour la manipulation du DOM
+         * ===========================================================
+         */
         const containerCarrousel2 = document.getElementById("containerCarrousel2");
         const g = document.getElementById("flecheGauche");
         const d = document.getElementById("flecheDroite");
     
 
-        //je débute le comportement des flèches de nav du carrousel
+        /**===========================================================
+         * Ajout du comportement des flèches du carrousel
+         * ===========================================================
+         */
         comportementFleche();
 
-        //taille du container en fonction du nombre d'images
+        /**===========================================================
+         * Définition de la taille du container en fonction du nombre
+         * d'images présentes dans le carrousel
+         * ===========================================================
+         */
         containerCarrousel2.style.width = (210*nombreImg)+"px"; 
 
-        //je crée les images
+        /**===========================================================
+         * Création des images avec la boucle for
+         * ===========================================================
+         */
         for(let i = 0; i < nombreImg; i++) {
             img = document.createElement('img');
             img.className = "photo";
@@ -49,7 +78,10 @@ dataFetch()
             containerCarrousel2.appendChild(img)
         }
 
-        //je crée le déplacement quand je clique à gauche
+        /**===========================================================
+         * Création du déplacement lors du click à gauche
+         * ===========================================================
+         */
         g.onclick = function(){
             if(pos > -19) {
                 pos--;
@@ -59,7 +91,10 @@ dataFetch()
             comportementFleche()
         }
 
-        //je crée le déplacement quand je clique à droite
+        /**===========================================================
+         * Création du déplacement lors du click à droite
+         * ===========================================================
+         */
         d.onclick = function(){
             if(pos < 0) {
                 pos++;
@@ -69,24 +104,39 @@ dataFetch()
             comportementFleche()
         }
 
-        //image cliquable 
+        /**===========================================================
+         * Sélection des éléments pour manipuler le DOM + création de 
+         * la boucle afin de complétement le modal
+         * ===========================================================
+         */
         const modal = document.querySelector('.modal');
         const close = document.querySelector('.btn-close');
         const closeBtn = document.querySelector('.btn');
-        const pictures = document.getElementsByClassName("photo");
-
-        
+        const pictures = document.getElementsByClassName("photo");        
 
         for(let j = 0; j < nombreImg ; j++) {
             pictures[j].onclick = function(){
-            //j'ajoute le titre + popularité du film
+            /**====================================================
+             * Ajout du titre et de la popularité du film
+             * ===================================================
+             */
             document.querySelector(".modal-title").innerHTML = `"` + titleCarrousel2[j] + `" / Evaluation des spectateurs : ` + voteCarrousel2[j];
+
+            /**====================================================
+             * Ajout du synopsis de chaque film
+             * ===================================================
+             */
             //j'ajoute le synopsis
             document.querySelector(".modal-body").innerHTML = `"` + synopsisCarrousel2[j] + `"`
-            if (synopsisCarrousel2 === null) {
+            if (synopsisCarrousel2[j] === "") {
                 document.querySelector(".modal-body").innerHTML = "Synopsis à venir..."
             }
-            //comportement du modal en fonction du click sur l'image ou sur les boutons de fermeture
+
+            /**====================================================
+             * Comportement de la fenêtre modal en fonction du 
+             * click sur l'image ou sur le bouton de fermeture
+             * ===================================================
+             */
             modal.style.display="block"
             close.onclick = function(){
                 modal.style.display = "none"
@@ -96,7 +146,11 @@ dataFetch()
             }}
         }
 
-        //création de la fonction permettant d'afficher ou non les flèches
+        /**===========================================================
+         * Création de la fonction permettant d'afficher ou non les 
+         * flèches en début et fin de carrousel
+         * ===========================================================
+         */
         function comportementFleche(){
             if(pos==(-nombreImg+1)) {
                 g.style.visibility="hidden";
